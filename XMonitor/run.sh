26d08c73c5b47db76a024b5b7a21668ec8e9a720
yangyl <642960662@qq.com>
2025-03-17 14:02:17 +0800

    update XMonitor

--- XMonitor/run.sh
+++ XMonitor/run.sh
@@ -7,5 +7,5 @@ cd $WORK_PATH
 sleep 2
 export QT_QPA_PLATFORM_PLUGIN_PATH=/opt/Qt5.12.12/5.12.12/gcc_64/plugins/platforms
 export LD_LIBRARY_PATH=$WORK_PATH/Lib:$LD_LIBRARY_PATH
-nohup $WORK_PATH/XMonitor_0.9.1 -d -a admin -f $WORK_PATH/XMonitor.yml > $APP_LOG_PATH/run.log 2>&1 &
+nohup $WORK_PATH/XMonitor_0.9.2 -d -a admin -f $WORK_PATH/XMonitor.yml > $APP_LOG_PATH/run.log 2>&1 &
 echo $! > pid.txt
